75762d661dd6205aefc455d352bcbbf1a286afb7
Sulayman Ahmed Khan <hellosulayman@gmail.com>
2026-09-19 02:42:09 +0600

    Add ignoreBuildErrors to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    // Vercel build-এর সময় টাইপস্ক্রিপ্ট এরর বাইপাস করবে
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Build-এর সময় ESLint এরর বাইপাস করবে
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     instrumentationHook: true,
   },
